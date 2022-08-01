7d465784222de66331c9b61f79b10bc0c624af4d
CowsArCool <67346720+CowsArCool@users.noreply.github.com>
2022-08-01 15:48:57 -0700

    Built hero info section

---
+++ src/ts/App.tsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+
+import '../scss/hero/Hero.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <div className="App-body">
+        <div className="hero">
+          <div className="section-info">
+            <div className="info-wrapper">
+              <h1>Michael <br />Dickinson</h1>
+              <div className="horizontal-line" />
+              <p>Exercitation consequat tempor aliquip voluptate et aute consequat cupidatat ipsum consectetur Lorem consequat. Ea exercitation nulla magna sunt nostrud qui veniam cillum. Incididunt nostrud non proident ullamco. Minim enim adipisicing mollit sunt commodo exercitation dolor sunt irure laboris.</p>
+            </div>
+            <button>Projects</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
