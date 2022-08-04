40ddbb604032bc13bd023636bfbed0eb863ef810
CowsArCool <67346720+CowsArCool@users.noreply.github.com>
2022-08-04 10:39:30 -0700

    created nmt project

---
+++ src/ts/projects/nmt_project.tsx
@@ -0,0 +1,22 @@
+import TranslationPanel from '../../assets/translation_panel.png';
+import VsCodeLogo from '../../assets/vscode_logo.png';
+import GithubLogo from '../../assets/git_logo.png';
+function NmtProject() {
+    return (
+        <div className="nmt-project">
+            <div className="info-section">
+                <h2>Neural Machine Translation</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque placerat et et leo pulvinar platea. At id laoreet sodales lacus dolor porta a scelerisque. Tortor diam non.</p>
+                <div className="links-container">
+                    <img src={GithubLogo}></img>
+                    <img src={VsCodeLogo}></img>
+                </div>
+            </div>
+            <div className='graphic-section'>
+                <img src={TranslationPanel} />
+            </div>
+        </div>
+    )
+}
+
+export default NmtProject;
