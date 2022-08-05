413960e935c2d48c91c809373c19a0dec0664733
CowsArCool <67346720+CowsArCool@users.noreply.github.com>
2022-08-04 17:04:58 -0700

    god knows

--- src/ts/projects/projects.tsx
+++ src/ts/projects/projects.tsx
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Project from "./project";
 import SidebarBlock from "./sidebar_block";
 
 function Projects({ children, sidebarThumbnails }: { children: JSX.Element[], sidebarThumbnails: React.ReactNode[] }) {
-    const [activeProject, setActiveProject] = useState(1);
+    const [activeProject, setActiveProject] = useState(0);
 
     const wrappedChildren = children.map((child, index) =>
-        <Project active={(activeProject == index) ? 'active' : (activeProject < index) ? 'after' : 'before'}>
+        <Project active={(activeProject == index) ? 'active' : (activeProject < index) ? 'after' : 'before'} key={index}>
             {child}
         </Project>
     )
 
     const sidebarBlocks = children.map((_, index) =>
-        <SidebarBlock image={sidebarThumbnails[index]} onClick={() => setActiveProject(index)} active={(activeProject == index)} />)
+        <SidebarBlock image={sidebarThumbnails[index]} onClick={() => setActiveProject(index)} active={(activeProject == index)} key={index} />)
 
     return (
-        <div className="projects">
+        <section className="projects scroll-section">
             <div className="sidebar">
                 <div className="blocks-container">
                     {sidebarBlocks}
@@ -27,10 +27,12 @@ function Projects({ children, sidebarThumbnails }: { children: JSX.Element[], si
                 </div>
                 <div style={{ height: '150vh', }}></div>
             </div>
-        </div>
+        </section>
     )
 
 }
 
 
 export default Projects
+
+
