ce07b52efa687f3c6f153163af63f0904a47e982
CowsArCool <67346720+CowsArCool@users.noreply.github.com>
2022-08-05 10:35:46 -0700

    added remaining project thumbnails

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -7,6 +7,9 @@ import staticData from '../data.json';
 
 import bertThumbnail from "../assets/bert_thumbnail.png";
 import NmtThumbnail from "../assets/nmt_thumbnail.png";
+import MlGuiThumbnail from "../assets/mlgui_thumbnail.png";
+import ClassificationThumbnail from "../assets/classification_thumbnail.png";
+
 import Hero from './hero/hero';
 import Projects from './projects/projects';
 import BertProject from './projects/bert-project';
@@ -14,14 +17,14 @@ import NmtProject from './projects/nmt_project';
 import Contact from './contact/Contact';
 import MlGuiProject from './projects/mlgui_project';
 import ClassificationProject from './projects/classification_project';
-function App() {
 
+function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <div className="App-body">
         <Hero bio={staticData.hero_bio} />
-        <Projects sidebarThumbnails={[bertThumbnail, NmtThumbnail, bertThumbnail, bertThumbnail]}>
+        <Projects sidebarThumbnails={[bertThumbnail, NmtThumbnail, MlGuiThumbnail, ClassificationThumbnail]}>
           <BertProject />
           <NmtProject />
           <MlGuiProject />
