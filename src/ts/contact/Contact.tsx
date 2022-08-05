b5f9d01063851e4b4c411a840f9e610a886d86c2
CowsArCool <67346720+CowsArCool@users.noreply.github.com>
2022-08-05 09:52:02 -0700

    implemented mailto in contact form

--- src/ts/contact/Contact.tsx
+++ src/ts/contact/Contact.tsx
@@ -1,17 +1,32 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
 function Contact({ email, message, description }: { email: string, message: string, description: string }) {
+    type formInputs = {
+        name: string,
+        email: string;
+        message: string;
+    }
+
+    const { register, handleSubmit, formState: { errors } } = useForm<formInputs>();
+    const onSubmit: SubmitHandler<formInputs> = data => window.location.href = (
+        `mailto:${email}?subject=contact from ${data.name}&body=${data.message}`
+    );
+
     return (
         <section className="contact-container scroll-section">
-            <h2>I'm always down to talk<br />Send me a message any time!</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <form>
+            <h2>{message}</h2>
+            <p>{description}</p>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="id-form-container">
                     <p>Name</p>
                     <p>Email</p>
-                    <input type="text" className="contact-input" placeholder="Enter your name" />
-                    <input type="text" className="contact-input" placeholder="Enter your email" />
+                    <input type="text" className="contact-input" placeholder="Enter your name" {...register('name')} />
+                    <input type="text" className="contact-input" placeholder="Enter your email" {...register('email')} />
                 </div>
                 <p>Message</p>
-                <input type="text" className='contact-input' placeholder="Enter your message" />
+                <input type="text" className='contact-input' placeholder="Enter your message" {...register('message', { required: true })} />
+                {errors.message && <p>This field is required</p>}
+
                 <input type="submit" className='submit-button' value='Submit' />
             </form>
         </section>
